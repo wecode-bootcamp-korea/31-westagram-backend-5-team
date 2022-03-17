930e4e70f5e6d568141b7e55607acb0d5dfa2b30
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-03-17 13:24:58 +0900

    project initial settings

---
+++ students/jeonghyeon/my_setting.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'django-insecure-+nbtto)2m@3)*ej0_@*_c@j%)e-la+-yg8o4bi6oqb*6k3ct8@'
+
+
+
+
+DATABASES = {
+   	'default' : {
+    	'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'westagram',
+        'USER': 'root',
+        'PASSWORD': 'ghkdwjdgus12!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'}
+    }
+   }
