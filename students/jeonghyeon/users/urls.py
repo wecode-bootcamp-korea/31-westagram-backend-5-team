5a1377478e5d7c38b510ac2101c0208e7d68aa82
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-03-18 16:45:16 +0900

    [DELETE] other URL

--- students/jeonghyeon/users/urls.py
+++ students/jeonghyeon/users/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views      import SignUpView,LoginView
 
 urlpatterns = [
-    path('signup',SignUpView.as_view()),
-    path('signin',LoginView.as_view())    
+    path('signup',SignUpView.as_view()), 
 ]
