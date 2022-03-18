18b1a1efc39b47d5bb7fc749dbfe9ffe9ca3a1c8
sangahnhan <sangahn0524@gmail.com>
2022-03-18 13:19:58 +0900

    Add: users 앱 생성, users.models 모델링 구현

---
+++ students/sangahn/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    name        = models.CharField(max_length=45)
+    email       = models.EmailField(max_length=45, unique=True)
+    password    = models.CharField(max_length=1000)
+    phonenumber = models.CharField(max_length=45)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'users'
