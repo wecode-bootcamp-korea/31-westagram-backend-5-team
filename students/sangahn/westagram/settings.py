ade22961b92a912bd061e8ce3ef14edc7c77f54c
sangahnhan <sangahn0524@gmail.com>
2022-03-22 17:39:47 +0900

    Add: jwt 발급

--- students/sangahn/westagram/settings.py
+++ students/sangahn/westagram/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
-from my_settings import DATABASES, SECRET_KEY
+from pathlib     import Path
+from my_settings import *
 
 import pymysql
 
